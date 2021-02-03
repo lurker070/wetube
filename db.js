7778730f10d2c916053616e129eae36394147fea
lurker070 <lurker070@github.com>
2021-02-03 20:16:12 +0900

    fourth commit

--- db.js
+++ db.js
@@ -1,38 +1,20 @@
-export const videos = [
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
     {
-        id:324393,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 12121,
-            name:"me",
-            email:"lurker070@gmail.com"
-        }
-    },
-    {
-        id:322233,
-        title: 'Video super',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 12121,
-            name:"me",
-            email:"lurker070@gmail.com"
-        }
-    },
-    {
-        id:321193,
-        title: 'Video nice',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 12121,
-            name:"me",
-            email:"lurker070@gmail.com"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
     }
-]
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = (error) => console.log(`Error on DB Connection ${error}`);
+
+db.once("open",handleOpen);
+db.on("error", handleError);
