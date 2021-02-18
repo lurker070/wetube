83d8222c1a899b759db2fd592598097b5884acf3
lurker070 <lurker070@github.com>
2021-02-18 17:55:49 +0900

    delete comment (bug fix-2)

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -52,6 +52,7 @@ export const videoDetail = async(req,res) => {
     const video = await Video.findById(id)
         .populate("creator")
         .populate("comments");
+    console.log(video.creator.id);
     res.render("videoDetail", { pageTitle: video.title, video });
     } catch(error) {
         console.log(error);
